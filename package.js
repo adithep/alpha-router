911be7cad1239e2dc443f1044fb52d1076c9dd40
Adithep <badsxx@gmail.com>
2014-08-09 12:40:30 +0700

    meme

--- package.js
+++ package.js
@@ -1,14 +1,21 @@
 Package.describe({
-  summary: "Experimental Routing"
+  summary: "Experimental Routing",
+  version: "0.0.1"
 });
 
 Package.on_use(function (api, where) {
-  api.use(['coffeescript', 'standard-app-packages', 'core-lib', 'alpha-template']);
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
+  api.use([
+    'bads:alpha-template',
+    'bads:core-lib',
+    'coffeescript',
+    'standard-app-packages'
+  ]);
   api.add_files('alpha-router.coffee', 'client');
 });
 
 Package.on_test(function (api) {
-  api.use('alpha-router');
+  api.use("../packages/bads:alpha-router");
 
   api.add_files('alpha-router_tests.js', ['client', 'server']);
 });
